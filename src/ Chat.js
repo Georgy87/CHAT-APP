2c5ff43fcc8e5dc95ae0d672e7179d0ffcdabb05
GEORGY <goshana87@mail.ru>
2020-10-18 20:55:24 +0300

    chat body and chat footer css

--- src/ Chat.js
+++ src/ Chat.js
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { Avatar, IconButton } from "@material-ui/core";
-import AttachFileIcon from '@material-ui/icons/AttachFile';
+import AttachFileIcon from "@material-ui/icons/AttachFile";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 import "./Chat.css";
 
 function Chat() {
     const [seed, setSeed] = useState("");
+    const [input, setInput] = useState("");
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000));
     }, []);
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+        console.log(input)
+    };
+
     return (
         <div className="chat">
             <div className="chat__header">
@@ -33,8 +42,28 @@ function Chat() {
                     </IconButton>
                 </div>
             </div>
-            <div className="chat__body"></div>
-            <div className="chat__footer"></div>
+            <div className="chat__body">
+                <p className={`chat__message ${true && "chat__reciever"}`}>
+                    <span className="chat__name">Georgy</span>
+                    Hey
+                    <span className="chat__timestamp">3:52pm</span>
+                </p>
+            </div>
+            <div className="chat__footer">
+                <InsertEmoticonIcon />
+                <form action="">
+                    <input
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        placeholder="Type a message"
+                        type="text"
+                    />
+                    <button onClick={sendMessage} type="submit">
+                        Send a message
+                    </button>
+                </form>
+                <MicIcon />
+            </div>
         </div>
     );
 }
--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import "./App.css";
 function App() {
     return (
         <div className="app">
-            <h1>Hello</h1>
             <div className="app__body">
                 <Sidebar />
                 <Chat />
